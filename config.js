60409b5ef8c94428f60fdeeb3cb66d6d7f950bad
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-06-28 00:15:23 +0200

    Task 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+    'GOOGLE_CLIENT_ID': '50855832923-0a64fa6cn3klkofstemm56mj2k57pqhe.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'WllG2WlM9pbdosQxYApeP1o0' ,
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
